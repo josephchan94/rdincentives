2dc43f87252a9d0c8daf28bcd6ddf64a027749c8
josephchan94 <jchan@media.ucla.edu>
2015-12-01 18:49:31 -0800

    trying to get carousel to progress to work

--- js/carousel.js
+++ js/carousel.js
@@ -5,3 +5,15 @@ $(document).ready(function() {
     interval: 4000,
   });
 });
+
+$('#quote-carousel').on('slide.bs.carousel', function (event) {
+  var curr = parseInt($('.progress-bar').attr('aria-valuenow'));
+
+  console.log(typeof curr);
+  if(event.direction == "right") {
+    $('.progress-bar').attr('aria-valuenow', curr - 1);
+  } else {
+    console.log(curr);
+    $('.progress-bar').attr('aria-valuenow', curr + 1);
+  }
+});
