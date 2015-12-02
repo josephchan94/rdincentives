c0761f14c489b0be567c15a226784e79f52627fa
josephchan94 <jchan@media.ucla.edu>
2015-12-01 21:34:54 -0800

    Revert "Revert "fk""
    This reverts commit df0612d8ac38f5473de8b47939cd5a9fa269d956.

--- js/carousel.js
+++ js/carousel.js
@@ -2,6 +2,6 @@ $(document).ready(function() {
   //Set the carousel options
   $('#quote-carousel').carousel({
     pause: true,
-    interval: 4000,
+    interval: 5000,
   });
 });
--- js/progress.js
+++ js/progress.js
@@ -1,10 +1,29 @@
-$('input').on('click', function(){
-  var valeur = 0;
-  $('input:checked').each(function(){
-       if ( $(this).attr('value') > valeur )
-       {
-           valeur =  $(this).attr('value');
-       }
+$(function() {
+    $( "#progressbar" ).progressbar({
+      value: 50
+    })
+    .data("value","50");
+
+    $("#plus").click(function() {
+        var currValue = $( "#progressbar" ).data("value");
+        currValue = parseInt(currValue) ? parseInt(currValue) : 0;
+        if(currValue <= 100) {
+            $( "#progressbar" ).progressbar({
+              value: currValue+1
+            }).data("value",currValue+1);
+            $("#progressLabel").html((currValue+1)+"%");
+        }
+    });
+
+    $("#minus").click(function() {
+        var currValue = $( "#progressbar" ).data("value");
+        currValue = parseInt(currValue) ? parseInt(currValue) : 0;
+        if(currValue > 0) {
+            $( "#progressbar" ).progressbar({
+              value: currValue-1
+            }).data("value",currValue-1);
+            $("#progressLabel").html((currValue-1)+"%");
+        }
+    });
+
   });
-  $('.progress-bar').css('width', valeur+'%').attr('aria-valuenow', valeur);
-});
