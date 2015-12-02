8d324f239a2190e9c0c9c8a40b17e653704cc251
josephchan94 <jchan@media.ucla.edu>
2015-12-01 23:16:02 -0800

    Revert "removed more junk"
    This reverts commit 0763d63f602ee751613ee425b79f16458e7aae33.

--- js/calc.js
+++ js/calc.js
@@ -1,7 +1,8 @@
-angular.module('CalculatorApp', []).controller('CalculatorController', function($scope) {
-  $scope.result = function() {
-    if ($scope.a + $scope.b + $scope.c > 0) {
-      return $scope.a + $scope.b + $scope.c;
-    }
-  };
-});
+angular.module('CalculatorApp', [])
+    .controller('CalculatorController', function($scope) {
+        $scope.result = function() {
+            if ($scope.a + $scope.b + $scope.c > 0) {
+                return $scope.a + $scope.b + $scope.c;
+            }
+        };
+    });
