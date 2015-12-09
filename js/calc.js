e57f2b46b3abc7061752a8852952a6b2747f6133
josephchan94 <jchan@media.ucla.edu>
2015-12-08 22:09:02 -0800

    calculator fix

--- js/calc.js
+++ js/calc.js
@@ -1,8 +1,9 @@
-angular.module('CalculatorApp', [])
-    .controller('CalculatorController', function($scope) {
-        $scope.result = function() {
-            if ($scope.a + $scope.b + $scope.c > 0) {
-                return $scope.a + $scope.b + $scope.c;
-            }
-        };
-    });
+function calc() {
+  num1 = parseFloat(document.getElementById('employee').value) || 0,
+  num2 = parseFloat(document.getElementById('annualWage').value) || 0,
+  num3 = parseFloat(document.getElementById('annualRev').value) || 0,
+  // retrieves the result element
+  added = document.getElementById('result');
+  added.textContent = num1+num2+num3;
+  console.log(added.textContent);
+}
