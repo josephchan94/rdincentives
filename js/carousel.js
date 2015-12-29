9b2e6e6539cd9442e39a10f39d8af75d346e99f7
josephchan94 <jchan@media.ucla.edu>
2015-12-29 12:22:47 -0800

    calc & fade scroll timing update

--- js/carousel.js
+++ js/carousel.js
@@ -2,7 +2,7 @@ $(document).ready(function() {
   //Set the carousel options
   $('#quote-carousel').carousel({
     pause: true,
-    interval: 6000,
+    interval: 8000,
   });
 });
 $('#quote-carousel').on('slide.bs.carousel', function (event) {
--- js/fade.js
+++ js/fade.js
@@ -6,7 +6,7 @@ $(document).ready(function() {
         /* Check the location of each desired element */
         $('.hideme').each( function(i){
 
-            var bottom_of_object = $(this).offset().top + $(this).outerHeight() /4;
+            var bottom_of_object = $(this).offset().top + $(this).outerHeight() /2;
             var bottom_of_window = $(window).scrollTop() + $(window).height();
 
             /* If the object is completely visible in the window, fade it */
