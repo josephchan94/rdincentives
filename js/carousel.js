d2b87e8199d82ba0290332dc04066f327586d160
josephchan94 <jchan@media.ucla.edu>
2015-12-29 04:58:38 -0800

    styling updatesz

--- js/carousel.js
+++ js/carousel.js
@@ -2,7 +2,7 @@ $(document).ready(function() {
   //Set the carousel options
   $('#quote-carousel').carousel({
     pause: true,
-    interval: 4000,
+    interval: 6000,
   });
 });
 $('#quote-carousel').on('slide.bs.carousel', function (event) {
