b833748fc9b82d7033e5d34be7e11aa92af38650
josephchan94 <jchan@media.ucla.edu>
2015-12-29 00:43:27 -0800

    Revert "merge conflict resolved and updates"
    This reverts commit 92e41df4331f183bf3871a04a6e9d92c75f6e174, reversing
    changes made to f2752dd03152a038ff5de3977a73685c0ba38365.

--- js/time.js
+++
@@ -1,5 +0,0 @@
-var today = new Date();
-var year = today.getFullYear();
-
-var el = document.getElementById("footer");
-el.innerHTML = '<br><p class="text-center">Copyright &copy;' + year + '<br>R&D Incentives Group '+ '</p>';
